8c3496cc72334f9cf6725d3ac9ce08860660efb3
skleinjung <skleinjung@gmail.com>
2016-03-12 02:53:30 -0600

    Implemented desktop versions of touch manager and sound manager. Wired up desktop version of Jounce.

---
+++ jounce-desktop/src/main/java/com/thrashplay/jounce/desktop/DesktopJounce.java
@@ -0,0 +1,104 @@
+package com.thrashplay.jounce.desktop;
+
+import com.thrashplay.jounce.Jounce;
+import com.thrashplay.jounce.Rectangle;
+import com.thrashplay.jounce.entity.Player;
+import com.thrashplay.luna.api.input.TouchManager;
+import com.thrashplay.luna.api.sound.SoundManager;
+import com.thrashplay.luna.desktop.LunaCanvas;
+import com.thrashplay.luna.desktop.input.MouseTouchManager;
+import com.thrashplay.luna.desktop.sound.DesktopSoundManager;
+import com.thrashplay.luna.math.Floats;
+
+/**
+ * TODO: Add class documentation
+ *
+ * @author Sean Kleinjung
+ */
+public class DesktopJounce implements Jounce {
+    private Player lastPlayerToScore = null;
+    private int leftPlayerScore = 0;
+    private int rightPlayerScore = 0;
+
+    private LunaCanvas canvas;
+    private TouchManager touchManager;
+    private SoundManager soundManager;
+
+    public DesktopJounce(LunaCanvas canvas) {
+        this.canvas = canvas;
+        this.touchManager = new MouseTouchManager(canvas);
+        this.soundManager = new DesktopSoundManager();
+    }
+
+    @Override
+    public TouchManager getTouchManager() {
+        return touchManager;
+    }
+
+    @Override
+    public SoundManager getSoundManager() {
+        return soundManager;
+    }
+
+    @Override
+    public Player getLastPlayerToScore() {
+        return lastPlayerToScore;
+    }
+
+    @Override
+    public int getLeftPlayerScore() {
+        return leftPlayerScore;
+    }
+
+    @Override
+    public void addPointForLeftPlayer() {
+        leftPlayerScore++;
+        lastPlayerToScore = Player.Left;
+    }
+
+    @Override
+    public int getRightPlayerScore() {
+        return rightPlayerScore;
+    }
+
+    @Override
+    public void addPointForRightPlayer() {
+        rightPlayerScore++;
+        lastPlayerToScore = Player.Right;
+    }
+
+    public void clearScores() {
+        rightPlayerScore = leftPlayerScore = 0;
+        lastPlayerToScore = null;
+    }
+
+    @Override
+    public Rectangle getGameBoardDimensions() {
+        float aspectRatio = (float) canvas.getWidth() / (float) canvas.getHeight();
+        float fourByThree = 4f / 3f;
+        int width = 0;
+        int height = 0;
+        if (Floats.areApproximatelyEqual(aspectRatio, fourByThree)) {
+            width = canvas.getWidth();
+            height = canvas.getHeight();
+        } else if (aspectRatio > fourByThree) {
+            height = canvas.getHeight();
+            width = (int) (height * fourByThree);
+        } else if (aspectRatio < fourByThree) {
+            width = canvas.getWidth();
+            height = (int) (width / fourByThree);
+        }
+
+        return new Rectangle((canvas.getWidth() - width) / 2, (canvas.getHeight() - height) / 2, width, height);
+    }
+
+    @Override
+    public float getWidthScalar() {
+        return getGameBoardDimensions().getWidth() / 959f;
+    }
+
+    @Override
+    public float getHeightScalar() {
+        return getGameBoardDimensions().getHeight() / 719f;
+    }
+}
---
+++ jounce-desktop/src/main/java/com/thrashplay/jounce/desktop/Main.java
@@ -0,0 +1,25 @@
+package com.thrashplay.jounce.desktop;
+
+import com.thrashplay.jounce.screen.JounceScreenManager;
+import com.thrashplay.luna.desktop.LunaCanvas;
+import com.thrashplay.luna.desktop.LunaWindow;
+import com.thrashplay.luna.engine.LunaGameConfig;
+
+/**
+ * TODO: Add class documentation
+ *
+ * @author Sean Kleinjung
+ */
+public class Main {
+    public static void main(String[] args) {
+        new LunaWindow("Jounce", 760, 480) {
+            @Override
+            protected LunaGameConfig createGameConfig(LunaCanvas canvas) {
+                LunaGameConfig gameConfig = new LunaGameConfig();
+                gameConfig.setScreenManager(new JounceScreenManager(new DesktopJounce(canvas)));
+                gameConfig.setDefaultScreen("title");
+                return gameConfig;
+            }
+        };
+    }
+}
